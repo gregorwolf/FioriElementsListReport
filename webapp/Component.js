80cd06a718d50c645b4f7ff7cea31781ac4fa482
S0015784785 <gregor.wolf@profes.de>
2018-03-09 18:04:24 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("FioriElementsListReport.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("FioriElementsListReport.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
